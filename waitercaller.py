dc528b21723c95839b89cdc8fbd8f84581ec4953
y1seve <yushuai09@gmail.com>
2017-05-31 21:39:11 +0800

    register a user and salt password

--- waitercaller.py
+++ waitercaller.py
@@ -11,9 +11,12 @@ from flask_login import login_user
 from flask_login import logout_user
 from flask_login import login_manager
 from mockdbhelper import MockDBHelper as DBHelper
+from passwordhelper import PasswordHelper
+
 
 
 DB = DBHelper()
+PH = PasswordHelper()
 app = Flask(__name__)
 login_manager = LoginManager(app)
 app.secret_key = 'tPXJY3X37Qybz4QykV+hOyUxVQeEXf1Ao2C8upz+fGQXKsM'
@@ -26,8 +29,13 @@ def home():
 def login():
     email = request.form.get('email')
     password = request.form.get('password')
-    user_password = DB.get_user(email)
-    if user_password and user_password == password:
+    # user_password = DB.get_user(email)
+    # if user_password and user_password == password:
+    #     user = User(email)
+    #     login_user(user, remember=True)
+    #     return redirect(url_for('account'))
+    stored_user = DB.get_user(email)
+    if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
         user = User(email)
         login_user(user, remember=True)
         return redirect(url_for('account'))
@@ -47,6 +55,20 @@ def load_user(user_id):
 @app.route('/logout')
 def logout():
     logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/register', methods=['POST'])
+def register():
+    email = request.form.get('email')
+    pw1 = request.form.get('password')
+    pw2 = request.form.get('password2')
+    if not pw1 == pw2:
+        return redirect(url_for('home'))
+    if DB.get_user(email):
+        return redirect(url_for('home'))
+    salt = PH.get_salt()
+    hashed = PH.get_hash(pw1 + salt)
+    DB.add_user(email, salt, hashed)
     return redirect(url_for('home'))
     
 if __name__ == '__main__':
